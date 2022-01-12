8902a2e2a6f8ff7d98e497f01ad35ad382a0a275
Pocco81 <pocco451@gmail.com>
2022-01-12 14:56:54 -0500

    fix: wrong root dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,17 +7,17 @@ if [ ! -z "${COLOR_VARIANTS:-}" ]; then
     IFS=', ' read -r -a _COLOR_VARIANTS <<<"${COLOR_VARIANTS:-}"
 fi
 
-cp -rf ${REPO_DIR}/src/sass/_tweaks.scss ${REPO_DIR}/src/sass/_tweaks-temp.scss
+cp -rf ${ROOT_DIR}/src/sass/_tweaks.scss ${ROOT_DIR}/src/sass/_tweaks-temp.scss
 
 for color in "${_COLOR_VARIANTS[@]}"; do
     echo "  + \e[0;32mGenerating\e[0m the 3.0 gtk${color}.css..."
-    sassc $SASSC_OPT ${REPO_DIR}/src/main/gtk-3.0/gtk${color}.{scss,css}
+    sassc $SASSC_OPT ${ROOT_DIR}/src/main/gtk-3.0/gtk${color}.{scss,css}
     echo "  + \e[0;32mGenerating\e[0m the 4.0 gtk${color}.css..."
-    sassc $SASSC_OPT ${REPO_DIR}/src/main/gtk-4.0/gtk${color}.{scss,css}
+    sassc $SASSC_OPT ${ROOT_DIR}/src/main/gtk-4.0/gtk${color}.{scss,css}
     echo "  + \e[0;32mGenerating\e[0m the 3.28 gnome-shell${color}.css..."
-    sassc $SASSC_OPT ${REPO_DIR}/src/main/gnome-shell/shell-3-28/gnome-shell${color}.{scss,css}
+    sassc $SASSC_OPT ${ROOT_DIR}/src/main/gnome-shell/shell-3-28/gnome-shell${color}.{scss,css}
     echo "  + \e[0;32mGenerating\e[0m the 40.0 gnome-shell${color}.css..."
-    sassc $SASSC_OPT ${REPO_DIR}/src/main/gnome-shell/shell-40-0/gnome-shell${color}.{scss,css}
+    sassc $SASSC_OPT ${ROOT_DIR}/src/main/gnome-shell/shell-40-0/gnome-shell${color}.{scss,css}
     echo "  + \e[0;32mGenerating\e[0m the cinnamon${color}.css..."
-    sassc $SASSC_OPT ${REPO_DIR}/src/main/cinnamon/cinnamon${color}.{scss,css}
+    sassc $SASSC_OPT ${ROOT_DIR}/src/main/cinnamon/cinnamon${color}.{scss,css}
 done
