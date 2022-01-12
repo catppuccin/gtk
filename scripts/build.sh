b282aa136fcdb4f986ee6522be612cbffd2df808
Pocco81 <pocco451@gmail.com>
2022-01-12 14:58:31 -0500

    fix: colors on build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,14 +10,14 @@ fi
 cp -rf ${ROOT_DIR}/src/sass/_tweaks.scss ${ROOT_DIR}/src/sass/_tweaks-temp.scss
 
 for color in "${_COLOR_VARIANTS[@]}"; do
-    echo "  + \e[0;32mGenerating\e[0m the 3.0 gtk${color}.css..."
+    echo -e "  + \e[0;32mGenerating\e[0m the 3.0 gtk${color}.css..."
     sassc $SASSC_OPT ${ROOT_DIR}/src/main/gtk-3.0/gtk${color}.{scss,css}
-    echo "  + \e[0;32mGenerating\e[0m the 4.0 gtk${color}.css..."
+    echo -e "  + \e[0;32mGenerating\e[0m the 4.0 gtk${color}.css..."
     sassc $SASSC_OPT ${ROOT_DIR}/src/main/gtk-4.0/gtk${color}.{scss,css}
-    echo "  + \e[0;32mGenerating\e[0m the 3.28 gnome-shell${color}.css..."
+    echo -e "  + \e[0;32mGenerating\e[0m the 3.28 gnome-shell${color}.css..."
     sassc $SASSC_OPT ${ROOT_DIR}/src/main/gnome-shell/shell-3-28/gnome-shell${color}.{scss,css}
-    echo "  + \e[0;32mGenerating\e[0m the 40.0 gnome-shell${color}.css..."
+    echo -e "  + \e[0;32mGenerating\e[0m the 40.0 gnome-shell${color}.css..."
     sassc $SASSC_OPT ${ROOT_DIR}/src/main/gnome-shell/shell-40-0/gnome-shell${color}.{scss,css}
-    echo "  + \e[0;32mGenerating\e[0m the cinnamon${color}.css..."
+    echo -e "  + \e[0;32mGenerating\e[0m the cinnamon${color}.css..."
     sassc $SASSC_OPT ${ROOT_DIR}/src/main/cinnamon/cinnamon${color}.{scss,css}
 done
