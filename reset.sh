2f03a9a40b597a238cb743e27156ad8f6a3d7e1f
Pranav <npv12@iitbbs.ac.in>
2022-12-02 01:23:56 +0530

    major cleanup

--- reset.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-git reset --hard
-rm -rf "${WORK_DIR}"
--- test.py
+++
@@ -1,9 +0,0 @@
-from scripts.var import *
-from scripts.dictionary import *
-from scripts.ctp_colors import *
-from scripts.default_colors import *
-
-test    = light_colors
-
-print(type(test))
-print(test)
