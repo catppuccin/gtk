cf30948cba22fa9f962bc1731c42ad82ac3e3d26
Pranav <npv12@iitbbs.ac.in>
2022-12-01 20:45:27 +0530

    color light too

--- scripts/recolor.py
+++ scripts/recolor.py
@@ -1,6 +1,6 @@
 from utils import replacetext
 from var import repo_dir, work_dir, src_dir, theme_name
-from ctp_colors import Color
+from ctp_colors import Color, latte
 from default_colors import *
 
 
@@ -12,9 +12,8 @@ def recolor(color: Color):
     replacetext(f"{work_dir}/install.sh", "Colloid", theme_name)
 
     print("MOD: Gtkrc.sh")
+    # Recolor as per accent for dark
     replacetext(f"{work_dir}/gtkrc.sh", "464646", "7c7f93")
-
-    # Recolor as per accent
     replacetext(f"{work_dir}/gtkrc.sh", default_blue, color.lavender)
     replacetext(f"{work_dir}/gtkrc.sh", default_purple, color.mauve)
     replacetext(f"{work_dir}/gtkrc.sh", default_pink, color.pink)
@@ -24,7 +23,17 @@ def recolor(color: Color):
     replacetext(f"{work_dir}/gtkrc.sh", default_green, color.green)
     replacetext(f"{work_dir}/gtkrc.sh", default_teal, color.teal)
 
-    # Recolor as per base
+    # Recolor as per base for light theme. hard code it as latte
+    replacetext(f"{work_dir}/gtkrc.sh", "DDDDDD", "45475a")
+    replacetext(f"{work_dir}/gtkrc.sh", "5b9bf8", latte.lavender)
+    replacetext(f"{work_dir}/gtkrc.sh", "BA68C8", latte.mauve)
+    replacetext(f"{work_dir}/gtkrc.sh", "F06292", latte.pink)
+    replacetext(f"{work_dir}/gtkrc.sh", "F44336", latte.red)
+    replacetext(f"{work_dir}/gtkrc.sh", "FB8C00", latte.peach)
+    replacetext(f"{work_dir}/gtkrc.sh", "FFD600", latte.yellow)
+    replacetext(f"{work_dir}/gtkrc.sh", "66BB6A", latte.green)
+    replacetext(f"{work_dir}/gtkrc.sh", "4DB6AC", latte.teal)
+
     replacetext(f"{work_dir}/gtkrc.sh", "background_light='#FFFFFF'",
                 "background_light='#eff1f5'")  # use latte_base for background_light
     replacetext(f"{work_dir}/gtkrc.sh", "background_dark='#0F0F0F'",
