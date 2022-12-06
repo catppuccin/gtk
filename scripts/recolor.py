b0ea2cc2304e2051aacf4d1a8655ff50b6116bb1
Pranav <npv12@iitbbs.ac.in>
2022-12-06 13:16:07 +0530

    feat: start using actual bg colors
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- scripts/recolor.py
+++ scripts/recolor.py
@@ -34,27 +34,27 @@ def recolor(color: Color, accent: str):
     recolor_accent(color, f"{work_dir}/gtkrc.sh", accent)
 
     replacetext(f"{work_dir}/gtkrc.sh", "background_light='#FFFFFF'",
-                "background_light='#eff1f5'")  # use latte_base for background_light
+                f"background_light='{latte.base}'")  # use latte_base for background_light
     replacetext(f"{work_dir}/gtkrc.sh", "background_dark='#0F0F0F'",
-                f"background_dark='#232634'")
+                f"background_dark='{color.base}'")
     replacetext(f"{work_dir}/gtkrc.sh", "background_darker='#121212'",
-                f"background_darker='#11111b'")
+                f"background_darker='{color.mantle}'")
     replacetext(f"{work_dir}/gtkrc.sh",
-                "background_alt='#212121'", "background_alt='#1e1e2e'")
+                "background_alt='#212121'", f"background_alt='{color.crust}'")
     replacetext(f"{work_dir}/gtkrc.sh", "titlebar_light='#F2F2F2'",
-                "titlebar_light='#dce0e8'")  # use latte_crust for titlebar_light
+                f"titlebar_light='{latte.crust}'")  # use latte_crust for titlebar_light
     replacetext(f"{work_dir}/gtkrc.sh", "titlebar_dark='#030303'",
-                f"titlebar_dark='#232634'")
+                f"titlebar_dark='{color.crust}'")
     replacetext(f"{work_dir}/gtkrc.sh", "background_dark='#2C2C2C'",
-                f"background_dark='#232634'")
+                f"background_dark='{color.base}'")
     replacetext(f"{work_dir}/gtkrc.sh", "background_darker='#3C3C3C'",
-                f"background_darker='#11111b'")
+                f"background_darker='{color.mantle}'")
     replacetext(f"{work_dir}/gtkrc.sh",
-                "background_alt='#464646'", "background_alt='#212121'")
+                "background_alt='#464646'", f"background_alt='{color.crust}'")
     replacetext(f"{work_dir}/gtkrc.sh",
-                "titlebar_light='#F2F2F2'", "titlebar_light='#dce0e8'")
+                "titlebar_light='#F2F2F2'", f"titlebar_light='{latte.crust}'")
     replacetext(f"{work_dir}/gtkrc.sh",
-                "titlebar_dark='#242424'", "titlebar_dark='#1e222a'")
+                "titlebar_dark='#242424'", f"titlebar_dark='{color.crust}'")
 
     print("Mod SASS Color_Palette_default")
     recolor_accent(
@@ -62,29 +62,29 @@ def recolor(color: Color, accent: str):
 
     # Greys
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
-                "grey-050: #FAFAFA", f"grey-050: #dce0e")
+                "grey-050: #FAFAFA", f"grey-050: {color.overlay2}")
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
-                "grey-100: #F2F2F2", f"grey-100: #e6e9ef")
+                "grey-100: #F2F2F2", f"grey-100: {color.overlay1}")
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
-                "grey-150: #EEEEEE", f"grey-150: #eff1f5")
+                "grey-150: #EEEEEE", f"grey-150: {color.overlay0}")
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
-                "grey-200: #DDDDDD", "grey-200: #ccd0da")  # Surface 0 Late
+                "grey-200: #DDDDDD", f"grey-200: {color.surface2}")  # Surface 0 Late
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
-                "grey-250: #CCCCCC", "grey-250: #bcc0cc")  # D = Surface 1 Late
+                "grey-250: #CCCCCC", f"grey-250: {color.surface1}")  # D = Surface 1 Late
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
-                "grey-650: #3C3C3C", "grey-650: #313244")  # H $surface $tooltip
+                "grey-650: #3C3C3C", f"grey-650: {color.surface0}")  # H $surface $tooltip
     replacetext(f"{src_dir}/sass/_color-palette-default.scss", "grey-700: #2C2C2C",
-                "grey-700: #1e1e2e")  # G $background; $base; titlebar-backdrop; $popover
+                f"grey-700: {color.base}")  # G $background; $base; titlebar-backdrop; $popover
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
-                "grey-750: #242424", "grey-750: #11111b")  # F $base-alt
+                "grey-750: #242424", f"grey-750: {color.crust}")  # F $base-alt
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
-                "grey-800: #212121", "grey-800: #11111b")  # E $panel-solid;
+                "grey-800: #212121", f"grey-800: {color.crust}")  # E $panel-solid;
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
-                "grey-850: #121212", "grey-850: #45475a")  # H Darknes
+                "grey-850: #121212", f"grey-850: {color.surface1}")  # H Darknes
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
-                "grey-900: #0F0F0F", "grey-900: #1e1e2e")  # G Darknes
+                "grey-900: #0F0F0F", f"grey-900: {color.base}")  # G Darknes
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
-                "grey-950: #030303", "grey-950: #11111b")  # F Darknes
+                "grey-950: #030303", f"grey-950: {color.crust}")  # F Darknes
 
     # Buttons
     replacetext(f"{src_dir}/sass/_color-palette-default.scss",
