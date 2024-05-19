4b73517739d1f055d166f69b1e43bad0f049d991
Isabel <isabel@isabelroses.com>
2024-05-19 17:51:50 +0100

    refactor: nix flake (#180)

--- flake.nix
+++ flake.nix
@@ -1,17 +1,18 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = import ./shell.nix { inherit pkgs; };
-      }
-    );
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./shell.nix { };
+      });
+    };
 }
-
--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
-{ pkgs ? import <nixpkgs> { } }:
-
+{
+  pkgs ? import <nixpkgs> { },
+}:
 pkgs.mkShell {
   name = "dev-shell";
   buildInputs = with pkgs; [
     python311
     python311Packages.catppuccin
+    sassc
   ];
 }
-
