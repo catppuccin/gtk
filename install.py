1a4a1cefe8b3a82e9047e14e0eed6d8f8299d77a
nullishamy <git@amyerskine.me>
2024-05-20 12:38:25 +0100

    fix: use rc2 in the install script

--- install.py
+++ install.py
@@ -75,7 +75,7 @@ def parse_args():
 
 def build_release_url(ctx: InstallContext) -> str:
     repo_root = "https://github.com/catppuccin/gtk/releases/download"
-    release = "v1.0.0-rc1"
+    release = "v1.0.0-rc2"
     zip_name = f"catppuccin-{ctx.flavor}-{ctx.accent}-standard+default.zip"
 
     return f"{repo_root}/{release}/{zip_name}"
@@ -88,7 +88,7 @@ def install(ctx: InstallContext):
     accent:     {ctx.accent}
     dest:       {ctx.dest.absolute()}
     link:       {ctx.link}
-    
+
     remote_url: {url}"""
     logger.info(build_info)
     httprequest = Request(url)
