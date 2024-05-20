13f2a344c855c2119b5e7e72e8b06bb98053de0f
nullishamy <git@amyerskine.me>
2024-05-20 02:10:54 +0100

    fix: use `v1.0.0-rc1` for the install script (#192)

--- install.py
+++ install.py
@@ -75,7 +75,7 @@ def parse_args():
 
 def build_release_url(ctx: InstallContext) -> str:
     repo_root = "https://github.com/catppuccin/gtk/releases/download"
-    release = "v1.0.0-alpha"
+    release = "v1.0.0-rc1"
     zip_name = f"catppuccin-{ctx.flavor}-{ctx.accent}-standard+default.zip"
 
     return f"{repo_root}/{release}/{zip_name}"
